ed39be4b342ce3be94cf8d8f2b91a1d09899c04b
JC Chang <jerryjcchang@gmail.com>
2018-11-14 19:11:24 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,5 +33,8 @@ def selective_tweet_shortener(tweet)
   tweet.split(" ").map do |phrase|
     if phrase.length > 140
       word_substituter(phrase)
-    elsif
+    elsif phrase.length <= 140
       phrase
+    end
+  end
+end
