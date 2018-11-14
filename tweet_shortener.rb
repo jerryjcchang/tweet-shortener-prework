031e6c7a22e3ab9e3f45f03ef4aa8965a70b177d
JC Chang <jerryjcchang@gmail.com>
2018-11-14 19:27:39 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,10 +39,11 @@ end
 
 
 def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).chars.length > 140
-    truncated = word_substituter(tweet).chars[0..136].push("...").join(" ")
-  else
-    tweet
-  end
-end
+  if word_substituter(tweet).chars.length > 140  
+    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
+  else 
+    word_substituter(tweet) 
+  end 
+end 
+
   
